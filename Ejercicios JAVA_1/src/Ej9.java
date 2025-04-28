2082f5f0eebf79c68708bce1022e2920bb7a4a3d
Dam <Dam@Info1-Pc09>
2025-04-28 12:10:42 +0200

    Ultimos ejercicios de figuras (9,10,11) y la creacion del menu basico de iniciacion

--- Ejercicios JAVA_1/src/Ej9.java
+++ Ejercicios JAVA_1/src/Ej9.java
@@ -3,6 +3,40 @@ import java.util.Scanner;
 public class Ej9 {
 	static Scanner entrada= new Scanner(System.in);
 	public static void main(String[] args) {
-		int cuadradro=0;
+		System.out.println("¿Cuanto de altura quieres que sea el rectángulo?");
+		int altura = entrada.nextInt();
+		System.out.println("¿Cuanto de ancho quieres que sea el rectángulo?");
+		int ancho = entrada.nextInt();
+		System.out.println("Rectángulo relleno");
+		dibujarRectangulo(ancho,altura);
+		System.out.println("Rectángulo vacío");
+		MarcoRectangulo(ancho,altura);
+		
+	}
+	public static void MarcoRectangulo(int ancho, int altura) {
+		for (int i =0;i<altura;i++) {
+			for(int j = 0;j<ancho;j++) {
+				if (i == 0 || i == altura-1||j == 0||j==ancho-1) {
+					System.out.print("* ");
+				}else{
+					System.out.print("  ");
+				}
+			}
+			System.out.println(" ");
+		}
+	}
+		
+	
+	
+	
+	public static void dibujarRectangulo(int ancho,int altura) {
+		for (int i =0;i<altura;i++) {
+			for(int j = 0;j<ancho;j++) {
+				System.out.print("* ");
+			}
+			System.out.println(" ");
+		}
+		
+		
 	}
 }
